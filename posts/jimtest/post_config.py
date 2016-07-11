d2704c636713d688dff05f62acbbcd16d79162c3
jimlondon <mail@jameshouston.com>
2016-07-11 06:47:51 +0100

    Updates for cats ride

--- posts/jimtest/post_config.py
+++
@@ -1,9 +0,0 @@
-COPY_GOOGLE_DOC_KEY = '1OLTamxMDavAzZApdmeZIapxYczOe5k77GZRC7SAdFv4'
-DEPLOY_SLUG = 'jimtest'
-NUM_SLIDES_AFTER_CONTENT = 2
-
-# Configuration
-AUDIO = False
-VIDEO = False
-FILMSTRIP = False
-PROGRESS_BAR = False
