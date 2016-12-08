b8e2cfd351119a6b5a60a7b49e144e085f120dcd
jimlondon <mail@jameshouston.com>
2016-12-08 18:44:35 +0000

    Social tags, updates for phonebook tour

---
+++ posts/london-police-boxes/post_config.py
@@ -0,0 +1,9 @@
+COPY_GOOGLE_DOC_KEY = '1ZqyHWk7C8odr-5yVz6et2u9iZmWpAhyhyt4Do8jMvCY'
+DEPLOY_SLUG = 'london-police-boxes'
+NUM_SLIDES_AFTER_CONTENT = 2
+
+# Configuration
+AUDIO = False
+VIDEO = False
+FILMSTRIP = False
+PROGRESS_BAR = False
